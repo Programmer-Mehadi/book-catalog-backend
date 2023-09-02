f9fc27963f23c4b2fccb860f8a9ae9f078a3044c
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-09-02 17:47:36 +0600

    [DONE] book api protected done.

--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -2,18 +2,21 @@ import express from 'express'
 import { BookController } from './book.controller'
 import validateRequest from '../../middlewares/validateRequest'
 import { BookValidation } from './book.validation'
+import { ENUM_USER_ROLE } from '../../../enums/user'
+import auth from '../../middlewares/auth'
 
 const router = express.Router()
 
 router.post(
   '/create-book',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(BookValidation.create),
   BookController.createBook
 )
 router.get('/', BookController.getAllBook)
 router.get('/:id', BookController.getSingleBook)
 router.get('/:categoryId/category', BookController.getBookByCategory)
-router.patch('/:id', BookController.updateBook)
-router.delete('/:id', BookController.deleteBook)
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), BookController.updateBook)
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), BookController.deleteBook)
 
 export const BookRoutes = router
