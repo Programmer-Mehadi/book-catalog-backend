05d9ba3741bc3046c15649b06bf373e4117e2a38
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-09-02 17:50:47 +0600

    [DONE] order api protected done.

--- src/app/modules/profile/profile.route.ts
+++ src/app/modules/profile/profile.route.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 import { ProfileController } from './profile.controller'
+import { ENUM_USER_ROLE } from '../../../enums/user'
+import auth from '../../middlewares/auth'
 
 const router = express.Router()
 
-router.get('/', ProfileController.getProfile)
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.CUSTOMER),
+  ProfileController.getProfile
+)
 
 export const ProfileRoutes = router
