a6698e07ef91cce243f8bed29c05e20634274d43
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-09-02 14:26:21 +0600

    [DONE] category api done.

---
+++ src/app/modules/category/category.controller.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from 'express'
+import { CategoryService } from './category.service'
+import sendResponse from '../../../shared/sendResponse'
+import httpStatus from 'http-status'
+import catchAsync from '../../../shared/catchAsync'
+
+const createCategory = catchAsync(async (req: Request, res: Response) => {
+  const result = await CategoryService.createCategory(req.body)
+  sendResponse<object>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Category created successfully',
+    data: result,
+  })
+})
+
+const getAllCategory = async (req: Request, res: Response) => {
+  const result = await CategoryService.getAllCategory()
+  sendResponse<object>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Categories retrieved successfully',
+    data: result,
+  })
+}
+
+const getSingleCategory = async (req: Request, res: Response) => {
+  const result = await CategoryService.getSingleCategory(req.params.id)
+  sendResponse<object>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Category fetched successfully',
+    data: result,
+  })
+}
+
+const updateCategory = async (req: Request, res: Response) => {
+  const result = await CategoryService.updateCategory(req.params.id, req.body)
+  sendResponse<object>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Category updated successfully',
+    data: result,
+  })
+}
+
+const deleteCategory = async (req: Request, res: Response) => {
+  const result = await CategoryService.deleteCategory(req.params.id)
+  sendResponse<object>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Category deleted successfully',
+    data: result,
+  })
+}
+export const CategoryController = {
+  createCategory,
+  getAllCategory,
+  getSingleCategory,
+  updateCategory,
+  deleteCategory,
+}
---
+++ src/app/modules/category/category.service.ts
@@ -0,0 +1,50 @@
+import prisma from '../../../shared/prisma'
+import { ICategory } from './category.interface'
+
+const createCategory = async (data: ICategory) => {
+  const result = await prisma.category.create({
+    data: data,
+  })
+  return result
+}
+
+const getAllCategory = async () => {
+  const result = await prisma.category.findMany()
+  return result
+}
+const getSingleCategory = async (id: string) => {
+  console.log(id)
+  const result = await prisma.category.findUnique({
+    where: {
+      id: id,
+    },
+  })
+  return result
+}
+
+const updateCategory = async (id: string, data: ICategory) => {
+  const result = await prisma.category.update({
+    where: {
+      id: id,
+    },
+    data: data,
+  })
+  return result
+}
+
+const deleteCategory = async (id: string) => {
+  const result = await prisma.category.delete({
+    where: {
+      id: id,
+    },
+  })
+  return result
+}
+
+export const CategoryService = {
+  createCategory,
+  getAllCategory,
+  getSingleCategory,
+  updateCategory,
+  deleteCategory,
+}
