c68e323af0df8c3a905d855576b17a70fc68a66d
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-09-03 15:13:26 +0600

    [DONE] order done/.

--- src/app/modules/profile/profile.controller.ts
+++ src/app/modules/profile/profile.controller.ts
@@ -2,15 +2,27 @@ import { Request, Response } from 'express'
 import { ProfileService } from './profile.service'
 import sendResponse from '../../../shared/sendResponse'
 import httpStatus from 'http-status'
+import ApiError from '../../../errors/ApiError'
 
 const getProfile = async (req: Request, res: Response) => {
-  const result = await ProfileService.getProfile('1')
-  sendResponse<object>(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'Profile retrieved successfully',
-    data: result,
-  })
+  if ('user' in req) {
+    const user = req.user as {
+      role?: string
+      userId?: string
+      iat?: number
+      exp?: number
+    }
+    const id = user.userId as string
+    const result = await ProfileService.getProfile(id)
+    sendResponse<object>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Profile retrieved successfully',
+      data: result,
+    })
+  } else {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Please login first')
+  }
 }
 
 export const ProfileController = {
--- src/app/modules/profile/profile.service.ts
+++ src/app/modules/profile/profile.service.ts
@@ -2,10 +2,20 @@ import { Prisma } from '@prisma/client'
 import prisma from '../../../shared/prisma'
 
 const getProfile = async (id: string) => {
-  // TODO: handle for admin and customer
   const result = await prisma.user.findUnique({
     where: {
       id: id,
+    },
+    select: {
+      name: true,
+      email: true,
+      role: true,
+      password: true,
+      contactNo: true,
+      address: true,
+      profileImg: true,
+      createdAt: true,
+      updatedAt: true,
     },
   })
   return result
